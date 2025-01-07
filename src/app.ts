f02ee0d412cbfb91ab0e28f370fd64a7c4228844
decoarc <anderarcodaci@gmail.com>
2025-01-07 15:21:49 -0300

    swagger commit

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,11 @@ import express, { Application } from "express";
 import cors from "cors";
 import routes from "./Interface/Routes";
 import { types } from "pg";
+import { setupSwagger } from "./Interface/swagger";
 
 types.setTypeParser(1700, (value) => parseFloat(value));
 
-const app: Application = express();
+const app = express();
 
 const corsOptions = {
   origin: "http://localhost:3000/",
@@ -21,5 +22,6 @@ app.use("/api", routes);
 app.get("/", (req, res) => {
   res.send("API");
 });
+setupSwagger(app);
 
 export default app;
